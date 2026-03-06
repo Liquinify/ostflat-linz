44a853461a12c8d4048563d5b672730b65bdcf19
liquinify <a.amin-afshar@globaltravel.space>
2026-03-06 20:43:23 +0100

    fix: Final merge conflict fix

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
 <script setup lang="ts">
-<<<<<<< HEAD
-=======
-import { Suspense } from "vue";
->>>>>>> d3a27523424809522c366ca52ca3d62085424778
 import LinzMap from "./components/Maps/LinzMap.vue";
 import MapSidebar from "./components/Sidebar/MapSidebar.vue";
 </script>
@@ -10,20 +6,7 @@ import MapSidebar from "./components/Sidebar/MapSidebar.vue";
 <template>
   <!-- <Header /> -->
   <main class="flex h-screen">
-<<<<<<< HEAD
     <MapSidebar />
-=======
-    <Suspense>
-      <template #default>
-        <MapSidebar />
-      </template>
-      <template #fallback>
-        <div class="flex items-center justify-center h-full w-full">
-          <p class="text-gray-500 text-lg">Loading...</p>
-        </div>
-      </template>
-    </Suspense>
->>>>>>> d3a27523424809522c366ca52ca3d62085424778
     <LinzMap />
   </main>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,12 @@ import "./style.css";
 import App from "./App.vue";
 import PrimeVue from "primevue/config";
 import Aura from "@primeuix/themes/aura";
-<<<<<<< HEAD
 import { createPinia } from "pinia";
 
 const pinia = createPinia();
 
 createApp(App)
   .use(pinia)
-=======
-
-createApp(App)
->>>>>>> d3a27523424809522c366ca52ca3d62085424778
   .use(PrimeVue, {
     theme: {
       preset: Aura,
